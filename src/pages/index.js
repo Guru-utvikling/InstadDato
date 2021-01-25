d2fe84a1b78652c1bb783bc94af7617307ba65cc
Guru-utvikling <damian@guru-utvikling.no>
2021-01-25 11:45:31 +0100

    init

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import Masonry from 'react-masonry-component'
+import Img from 'gatsby-image'
+import Layout from "../components/layout"
+
+const IndexPage = () => (
+  <Layout>
+  </Layout>
+)
+
+export default IndexPage
+
+export const query = graphql`
+
+`
